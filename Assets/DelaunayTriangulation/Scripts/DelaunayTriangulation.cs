74d20da757594bc8f2c20be7146d89d2465ffe3a
Vrtx Forge <epanlican@gmail.com>
2024-11-03 19:29:19 +0800

    super triangle drawn correctly

--- Assets/DelaunayTriangulation/Scripts/DelaunayTriangulation.cs
+++ Assets/DelaunayTriangulation/Scripts/DelaunayTriangulation.cs
@@ -12,6 +12,35 @@ public class DelaunayTriangulation : MonoBehaviour
     private List<Triangle> triangles = new List<Triangle>();
     private Triangle superTriangle;
 
+    public Vector2 simulationPosition;
+    public float triangleScale = 3;
+
+    private float simulationScale;
+
+    private void Start()
+    {
+        simulationScale = maxBound.x;
+    }
+
+    //needs to put this in the debugger
+    private void Update()
+    {
+        //debug super triangle
+        Triangle superTriangleVec2 = GenerateSuperTriangle(
+            triangleScale,
+            simulationScale,
+            simulationPosition
+        );
+
+        Vector3 tA = new(superTriangleVec2.A.x, 0, superTriangleVec2.A.y);
+        Vector3 tB = new(superTriangleVec2.B.x, 0, superTriangleVec2.B.y);
+        Vector3 tC = new(superTriangleVec2.C.x, 0, superTriangleVec2.C.y);
+
+        Debug.DrawLine(tA, tB, Color.red);
+        Debug.DrawLine(tB, tC, Color.red);
+        Debug.DrawLine(tC, tA, Color.red);
+    }
+
     private void GeneratePoints()
     {
         for (int i = 0; i < maxPointCount; i++)
@@ -106,6 +135,11 @@ public class DelaunayTriangulation : MonoBehaviour
 
             return det > 0;
         }
+
+        public bool Contains(Vector2 point)
+        {
+            return (A == point || B == point || C == point);
+        }
     }
 
     public class Edge
@@ -143,5 +177,8 @@ public class DelaunayTriangulation : MonoBehaviour
         {
             Gizmos.DrawSphere(new Vector3(bound.x, 0, bound.y), 0.2f);
         }
+
+        if (!Application.isPlaying && points == null)
+            return;
     }
 }
